ecc62f149f8ca7d63bfea2bf3a92b0168f074c01
Nicole Tsingas <nicolecass27@gmail.com>
2023-09-09 14:35:23 -0300

    Feito testes unitários do delete users

--- controller/__tests__/users.spec.js
+++ controller/__tests__/users.spec.js
@@ -5,30 +5,30 @@ const {
   updateUser,
   getUsers,
   // getUsersById,
-  // deleteUser,
+  deleteUser,
 } = require('../users');
 
 jest.mock('../../models/User');
 
-describe('Users controller', () => {
-  const req = {
-    body: {
-      id: '1',
-      email: 'test@example.com',
-      password: 'password',
-      role: 'admin',
-    },
-  };
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
-  const res = {
-    status: jest.fn().mockReturnThis(),
-    json: jest.fn(),
-  };
+const req = {
+  body: {
+    id: '1',
+    email: 'test@example.com',
+    password: 'password',
+    role: 'admin',
+  },
+};
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
+const res = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+};
 
+describe('createUser', () => {
   it('should success create a new user', async () => {
     bcrypt.hash = jest.fn().mockResolvedValue('hashedPassword');
     User.prototype.save = jest.fn().mockResolvedValue({});
@@ -63,7 +63,9 @@ describe('Users controller', () => {
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' });
   });
+});
 
+describe('updateUser', () => {
   it('should update user data', async () => {
     const req = {
       params: { id: '1' },
@@ -122,7 +124,9 @@ describe('Users controller', () => {
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' });
   });
+});
 
+describe('getUsers', () => {
   it('should return all users', async () => {
     const users = [{ email: 'waiter@example.com' }, { email: 'admin@example.com' }];
     const req = {};
@@ -140,45 +144,66 @@ describe('Users controller', () => {
     expect(res.status).toHaveBeenCalledWith(500);
     expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' });
   });
+});
 
-  /* it('should return user by id', async () => {
+/* describe('getUsersById', () => {
+  it('should return user by id', async () => {
+    const fakeUser = {
+      id: '1',
+    };
     const req = {
-      params: { id: '1' },
-      body: {
-        email: 'waiter@example.com',
-        password: 'password',
-        role: 'waiter',
+      params: {
+        id: '1',
       },
     };
-    const user = { id: '1', email: 'waiter@example.com' };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    User.findById = jest.fn().mockRejectedValue(user);
+    User.findById = jest.fn().mockRejectedValue(fakeUser);
     await getUsersById(req, res);
-    expect(User.findById).toHaveBeenCalledWith(user.id);
-    expect(res.json).toHaveBeenCalledWith({ findUser: user });
+    expect(User.findById).toHaveBeenCalledWith('1');
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({ findUser: fakeUser });
   });
+}); */
 
-   it('should handle error user not found', async () => {
-    const userId = 'invalidId';
-    const req = { params: { id: userId } };
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
+describe('deleteUser', () => {
+  it('should delete a user', async () => {
+    const fakeUser = {
+      id: '1',
     };
-    User.findById = jest.fn().mockRejectedValue(null);
-    await getUsersById(req, res);
-    expect(User.findById).toHaveBeenCalledWith(userId);
-    expect(res.status).toHaveBeenCalledWith(404);
-    expect(res.json).toHaveBeenCalledWith({ error: 'User not found' });
-  }); */
-/*
-  it('should delete an user', async () => {
-    req.params.id = 1;
+    const req = {
+      params: {
+        id: '1',
+      },
+    };
+    User.findByIdAndDelete = jest.fn().mockResolvedValue(fakeUser);
     await deleteUser(req, res);
+    expect(User.findByIdAndDelete).toHaveBeenCalledWith('1');
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith({ msg: 'Success deleted user' });
-  }); */
+  });
+
+  it('should handle error 404 if user not found', async () => {
+    const req = {
+      params: {
+        id: 'invalid_id',
+      },
+    };
+    User.findByIdAndDelete = jest.fn().mockResolvedValue(null);
+    await deleteUser(req, res);
+    expect(User.findByIdAndDelete).toHaveBeenCalledWith('invalid_id');
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ error: 'User not found' });
+  });
+
+  it('should handle error - deleteUser', async () => {
+    const req = {
+      params: {
+        id: 'user_id',
+      },
+    };
+    User.findByIdAndDelete = jest.fn().mockRejectedValue(new Error('Query error'));
+    await deleteUser(req, res);
+    expect(User.findByIdAndDelete).toHaveBeenCalledWith('user_id');
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({ error: 'Internal Server Error' });
+  });
 });
